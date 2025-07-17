430a06abff073624b01275a7d31b61d1a6b10194
dianniii <kropuhinskaya@yandex.ru>
2025-07-17 20:46:08 +0300

    updates

--- src/components/dasboard-layout/footer.tsx
+++ src/components/dasboard-layout/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="bg-emerald-100 mt-15">
             <div className="mx-auto max-w-screen-xl px-6 py-4 text-center text-sm text-black">
-                © 2025 Users
+                © 2025 Users List
             </div>
         </footer>
     );
