f657e4575c68b45678eb56b4d0b92d279bb25b6e
dianniii <kropuhinskaya@yandex.ru>
2025-07-17 21:15:02 +0300

    updates styles

--- src/components/ui/InputPoisk.tsx
+++ src/components/ui/InputPoisk.tsx
@@ -8,9 +8,7 @@ function InputPoisk ({ className, type, ...props }: React.ComponentProps<"input"
       type={type}
       data-slot="input"
       className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground border-emerald-200 flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "hover:border-emerald-100 focus-visible:border-emerald-100 focus-visible:ring-emerald-100 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 aria-invalid:border-destructive",
+        "border-emerald-200 h-9 w-full rounded-md border bg-transparent px-3 py-1 text-base shadow-xs outline-none transition-colors disabled:opacity-50 disabled:cursor-not-allowed hover:border-emerald-100 focus-visible:border-emerald-100 focus-visible:ring-emerald-100 focus-visible:ring-2",
         className
       )}
       {...props}
