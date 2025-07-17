58325b8042d87cfca3211ab3968bb143286c7214
dianniii <kropuhinskaya@yandex.ru>
2025-07-17 20:38:48 +0300

    updates

--- src/components/dasboard-layout/header.tsx
+++ src/components/dasboard-layout/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <header className="bg-emerald-100">
         <div className="mx-auto max-w-screen-xl px-6 flex h-18 items-center justify-between">
-            <Link href="/" className="text-xl font-bold text-black">Users</Link>
+            <Link href="/" className="text-xl font-bold text-black">Users List</Link>
         </div>
         </header>
     );
