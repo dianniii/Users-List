64304194fb7bfdec2a4702a572474b2edea3fbf4
dianniii <kropuhinskaya@yandex.ru>
2025-07-17 21:21:30 +0300

    updates styles

--- src/components/dasboard-layout/header.tsx
+++ src/components/dasboard-layout/header.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Header() {
     return (
         <header className="bg-emerald-100">
-        <div className="mx-auto max-w-screen-xl px-6 flex h-18 items-center justify-between">
+        <div className="mx-auto max-w-screen-xl px-6 flex h-18 items-center justify-start">
             <Link href="/" className="text-xl font-bold text-black">Users List</Link>
         </div>
         </header>
